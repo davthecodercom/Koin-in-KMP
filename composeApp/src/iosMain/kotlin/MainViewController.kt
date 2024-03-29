984c2a9dd09c3d335f32ddf448471e6e26a48c7b
David Cruz <david.cruz@davthecoder.com>
2024-03-29 16:29:57 +0700

    [iOS] Move start Koin to iOSApp as per docs

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -2,7 +2,6 @@ import androidx.compose.ui.window.ComposeUIViewController
 import di.initKoin
 
 fun MainViewController() = ComposeUIViewController {
-    initKoin()
     App(
         greeting = GreetingsHelper().name()
     )
